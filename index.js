83497f8198dbad603e6d2f5a2a52e6f3c6c0f4b7
Cals Ranna <cals@MacBook-Pro.local>
2020-09-15 01:37:37 +0800

    Publish to npmjs.

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
 const DBC = require("./lib/dbc");
 
-let start = Date.now();
-// let dbc = DBC.read("D:\\FoxWOW\\Server\\Core\\data\\dbc\\CreatureFamily.dbc");
-let dbc = DBC.read("/Users/cals/FoxWoW/Server/Core/data/dbc/GMSurveyAnswers.dbc");
-let end = Date.now();
-
-console.log(dbc);
-console.log(`Time used: ${end - start} milseconds.`);
+module.exports = DBC;
