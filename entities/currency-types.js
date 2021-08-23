c732a17c45f762bbdcc30f774258b744fdd9d078
CalsRanna <calsranna@gmail.com>
2021-08-23 23:15:22 +0800

    feat(entity): add currency definations

--- entities/currency-types.js
+++ entities/currency-types.js
@@ -1,8 +1,8 @@
 const t = require("../lib/types");
 
 module.exports = {
-  unk_0: t.int32le,
-  unk_1: t.int32le,
-  unk_2: t.int32le,
-  unk_3: t.int32le,
+  ID: t.int32le,
+  ItemID: t.int32le,
+  CategoryID: t.int32le,
+  BitIndex: t.int32le,
 };
